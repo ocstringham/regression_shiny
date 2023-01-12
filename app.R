cf44614232af293f07a32d95e4794ba86c233537
Oliver C. Stringham <oliverstringham@gmail.com>
2023-01-12 15:47:12 -0500

    u

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shinyjs)
 library(shinythemes)
 library(sortable)
 library(ggplot2)
+library(egg)
 
 example_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
@@ -39,13 +40,24 @@ ui <- fluidPage(
   # Viz data
   fluidRow(column(12, align = "left", h3(uiOutput("viz_title")))),
   fluidRow(column(12, align = "left", p(uiOutput("viz_text")))),
-  fluidRow(column(12, align = "center", uiOutput("plot_button"))),
+  # fluidRow(column(12, align = "center", uiOutput("plot_button"))),
+  fluidRow(column(12, align = "center", uiOutput("expl_var"))),
   fluidRow(column(12, align = "left", plotOutput("viz_plots"))),
   
-    
-  # # Buckets
-  # fluidRow(column(12, align = "center", uiOutput("buckets")))
-    
+  
+  # Build models
+  fluidRow(column(12, align = "left", h3(uiOutput("model_title")))),
+  fluidRow(column(12, align = "left", p(uiOutput("model_text")))),
+  fluidRow(column(12, align = "left", uiOutput("buckets1"))),
+  fluidRow(column(12, align = "left", uiOutput("buckets2"))),
+  # fluidRow(column(4, align = "center", uiOutput("buckets3")))
+  
+  
+  # View Output
+  fluidRow(column(12, align = "left", h3(uiOutput("output_title")))),
+  fluidRow(column(12, align = "left", p(uiOutput("output_text")))),
+  fluidRow(column(12, align = "left", p(uiOutput("model_output"))))
+  
 )
 
 #----------------------------------------------------------------------------- #
@@ -76,20 +88,7 @@ server <- function(input, output) {
     req(input_file())
     example_text})
   
-  ## button to hide/show table
-  
-  # ## only show when table is loaded
-  # output$show_button = renderUI({
-  #   req(input$file1)
-  #   actionButton(inputId = "button", label = "show / hide table")
-  # })
-  # 
-  # 
-  # ## observe the button being pressed
-  # observeEvent(input$button, {
-  #   shinyjs::toggle("table")
-  # })
-  
+
   
   ## table
   output$table <- DT::renderDataTable({
@@ -104,17 +103,17 @@ server <- function(input, output) {
   
   
   
-  # Select response variable, only show once user clicks I'm ready
+  # Select response variable
   output$response_var_title = renderText({
-    req(input$file1)
+    req(input_file())
     "3. Select independent variable"})
   
   output$response_var_text = renderText({
-    req(input$file1)
+    req(input_file())
     example_text})
   
   output$response_var = renderUI({
-    req(input$file1)
+    req(input_file())
     cols = colnames(input_file())
     radioButtons(inputId = "response_var", label = "Select an independent variable.",
              choices = cols, selected = character(0))
@@ -126,31 +125,32 @@ server <- function(input, output) {
   
   ## text
   output$viz_title = renderText({
-    req(input$file1)
+    req(input_file())
     "4. Vizualize data"})
   
   output$viz_text = renderText({
-    req(input$file1)
+    req(input_file())
     example_text})
+  
+  ## radio for expl var
+  output$expl_var = renderUI({
+    req(input_file())
+    req(getRV())
+    response = getRV()
+    data <- input_file()
+    expl = names(data)[! names(data) %in% response]
+    radioButtons(inputId = "expl_var", label = "Select an dependent variable to plot.",
+                 choices = expl, selected = character(0),  inline=T)
+  }
+  )
 
   
   ### get response var
   getRV <- reactive({ input$response_var})
+  getEV <- reactive({input$expl_var})
   
-  # ### observe the button being pressed
-  # 
-  # output$plot_button = renderUI({
-  #   req(input$file1)
-  #   actionButton(inputId = "plotButton", label = "Plot Data")
-  # })
-  # 
-  # 
-  # observeEvent(input$plot_button, {
-  #   shinyjs::toggle("viz")
-  # })
   
-  # output$viz = renderPrint(get)
-  
+  ## create plot
   output$viz_plots = renderPlot({
     
     ## reqs
@@ -160,66 +160,189 @@ server <- function(input, output) {
     ## get data
     data <- input_file()
 
-    ## get response variable
+    ## get response/expl variables
     response = getRV()
+    expl = getEV()
     
-    ## get all explanatory vars
-    expl = names(data)[! names(data) %in% response]
 
-    # ## plot random column
-    # ggplot(data, aes_string(y = response, x = expl[1])) +
-    #   geom_point()
+    ## plot
     
-    # create list of each plot
-    plot_list = list()
-    for(i in 1:expl){
+    ### if response is numeric
+    if(is.numeric(data[, response])){
       
-      # get data type: either numeric or character
+      if(is.numeric(data[, expl])){
+        ggplot(data, aes_string(y = response, x = expl)) +
+          geom_point() +
+          theme_presentation()
+      }else if(is.character(data[, expl])){
+        ggplot(data, aes_string(y = response, x = expl)) +
+          geom_boxplot() + 
+          theme_presentation()
+      }
       
-      
+    # if response is character
+    }else if(is.character(data[, response])){
+    
+      if(is.numeric(data[, expl])){
+        ggplot(data, aes_string(y = response, x = expl)) +
+          geom_boxplot() + 
+          theme_presentation()
+      }else if(is.character(data[, expl])){
+        ggplot(data, aes_string(group = response, fill = response, x = expl)) +
+          geom_bar(position = position_dodge2(preserve = "single")) + 
+          scale_fill_brewer(palette = 1, type = 'qual') +
+          theme_presentation()
+      }
     }
 
   })
   
   
-  # Drag and Drop Col names
-  output$buckets = renderUI( 
+  # Build and run models
+  
+  ## Text
+  output$model_title = renderText({
+    req(input_file())
+    "5. Build models"})
+  
+  output$model_text = renderText({
+    req(input_file())
+    example_text})
+    
+
+  
+  
+  ## Drag and Drop Col names
+  output$buckets1 = renderUI( 
       {
         # create list of colnames
-        req(input$file1)
-        data = input_file()
-        cols = colnames(data)
+        req(input_file())
+        req(getRV())
         
+        response = getRV()
+        data <- input_file()
+        cols = names(data)[! names(data) %in% response]
         
         # create bucket list
+        h4("Model 1")
         bucket_list(
           header = "Drag the items in any desired bucket",
-          group_name = "bucket_list_group",
+          group_name = "bucket_list1",
           orientation = "horizontal",
 
 
           add_rank_list(
             text = "Drag from here",
             labels = as.list(cols),
-            input_id = "rank_list_1",
-            css_id = "list1", 
-            options = sortable_options(
-              group = list(
-                pull = "clone",
-                put = FALSE))
-            ),
+            input_id = "b1_rank_list_1",
+            css_id = "b1_list1"),
 
           add_rank_list(
             text = "to here",
             labels = NULL,
-            input_id = "rank_list_2",
-            css_id = "list2")
+            input_id = "b1_rank_list_2",
+            css_id = "b1_list2")
       )
 
     })
   
   
+  output$buckets2 = renderUI( 
+    {
+      # create list of colnames
+      req(input_file())
+      req(getRV())
+      
+      response = getRV()
+      data <- input_file()
+      cols = names(data)[! names(data) %in% response]
+      
+      
+      # create bucket list
+      bucket_list(
+        header = "Drag the items in any desired bucket",
+        group_name = "bucket_list2",
+        orientation = "horizontal",
+        
+        add_rank_list(
+          text = "Drag from here",
+          labels = as.list(cols),
+          input_id = "b2_rank_list_1",
+          css_id = "b2_list1"),
+        
+        add_rank_list(
+          text = "to here",
+          labels = NULL,
+          input_id = "b2_rank_list_2",
+          css_id = "b2_list2")
+      )
+      
+    })
+  
+  
+  
+  # Model output
+  
+  ## text
+  output$viz_title = renderText({
+    req(input_file())
+    "6. Model Output"})
+  
+  output$viz_text = renderText({
+    req(input_file())
+    example_text})
+  
+  ## output
+  
+  ### get Vars used in models
+  getModel1 <- reactive({ input$b1_rank_list_2})
+  getModel2 <- reactive({ input$b2_rank_list_2})
+  
+  output$model_output = renderText( {
+    req(input_file())
+    req(getModel1())
+    model1 = getModel1()
+    model2 = getModel2()
+    paste(model1, model2)
+    
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+# # create list of each plot
+# plot_list = list()
+# for(i in 1:length(expl)){
+#   
+#   # get data type: either numeric or character
+#   if(is.numeric(data$expl[i])){
+#     plot_list[[i]] =
+#       renderPlot({
+#       ggplot(data, aes_string(y = response, x = expl[i])) +
+#       geom_point()
+#       })
+#     
+#   }else if(is.character(data$expl[i])){
+#     plot_list[[i]] =
+#       renderPlot({
+#       ggplot(data, aes_string(y = response, x = expl[i])) +
+#       geom_boxplot()
+#       })
+#   }
+# }
+
+# print(plot_list[[1]])
+# wrap_plots(plot_list, ncol = 3)
+# do.call(tagList, plot_list)
+
--- gen_data.R
+++ gen_data.R
@@ -1,5 +1,6 @@
 
 library(tidyverse)
+library(patchwork)
 
 # generate fake dataset, which different types of explanatory variable data types: categorical and numerics
 df = tibble(`birth year` = runif(100, min = 1900, max = 1960) %>% round()) %>% 
@@ -27,3 +28,30 @@ df %>%
   geom_boxplot()
 
 write.csv(df, file = 'fake data.csv', row.names = F)
+
+df = read.csv('fake data.csv')
+df
+is.numeric(df$age)
+
+
+ggplot(df, aes(group = sex, x = in.war, fill = sex)) + 
+  geom_bar(position = position_dodge2(preserve = "single")) + 
+  scale_fill_brewer(palette = 1, type = 'qual')
+
+ggplot(df, aes(x = in.war, y = age, fill = "gray")) + 
+  geom_boxplot() + 
+  scale_fill_brewer(palette = 3, type = 'qual')
+
+
+# p1 <- ggplot(mtcars) + geom_point(aes(mpg, disp))
+# p2 <- ggplot(mtcars) + geom_boxplot(aes(gear, disp, group = gear))
+# p3 <- ggplot(mtcars) + geom_bar(aes(gear)) + facet_wrap(~cyl)
+# p4 <- ggplot(mtcars) + geom_bar(aes(carb))
+# p5 <- ggplot(mtcars) + geom_violin(aes(cyl, mpg, group = cyl))
+# 
+# # Either add the plots as single arguments
+# wrap_plots(p1, p2, p3, p4, p5)
+# 
+# # Or add them as a list...
+# plots <- list(p1, p2, p3, p4, p5)
+# wrap_plots(plots)
